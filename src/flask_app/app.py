852ef1edeb5f7ab6cd707f316f7bf164d8f2fd4a
Roy Yosef <royyosef9@gmail.com>
2025-04-02 16:09:45 +0300

    roy prod

--- src/flask_app/app.py
+++ src/flask_app/app.py
@@ -8,24 +8,23 @@ REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
 
-# התחברות ל־Redis
 r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD, decode_responses=True)
 
 @app.route("/")
 def home():
-    visits = r.incr("counter")  # מגדיל את הערך בכל ביקור
-    greeting = os.getenv("APP_GREETING", "👋 ברירת מחדל")
+    visits = r.incr("counter")  
+    greeting = os.getenv("APP_GREETING", "ברירת מחדל")
     return f"{greeting} — Visit #{visits}"
 
 @app.route("/about")
 def about():
     user = os.getenv("USERNAME", "Anonymous")
-    return f"📦 This is RoyApp, running for user: {user}"
+    return f" This is RoyApp, running for user: {user}"
 
 @app.route("/secret")
 def secret():
-    api_key = os.getenv("API_KEY", "🚫 Not Set")
-    return f"🔐 Your API Key is: {api_key}"
+    api_key = os.getenv("API_KEY", " Not Set")
+    return f" Your API Key is: {api_key}"
 
 @app.route("/dev-check")
 def dev_check():
